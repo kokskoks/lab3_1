d62c5777a0f41f5357f9df5004b5021b980346fb
Kadłubek <179640@edu.p.lodz.pl>
2016-04-14 14:44:25 +0200

    added test for AddProductCommandHandler behaviour

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/AddProductCommandHandlerTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/AddProductCommandHandlerTest.java
@@ -6,21 +6,44 @@ import static org.mockito.Mockito.*;
 import org.junit.Before;
 import org.hamcrest.Matchers;
 
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
 import pl.com.bottega.ecommerce.sales.application.api.command.AddProductCommand;
 import pl.com.bottega.ecommerce.sales.application.api.handler.AddProductCommandHandler;
 
 public class AddProductCommandHandlerTest {
+
+    AddProductCommand addProductCommand;
+
+    @Before
+    public void setUp(){
+        addProductCommand = mock(AddProductCommand.class);
+
+        when(addProductCommand.getOrderId()).thenReturn(Id.generate());
+    }
+
 
     @Test(expected = NullPointerException.class)
     public void handle_checkForNullPointerExceptionBecauseOfNullPrivateDependencies(){
 
         AddProductCommandHandler addProductCommandHandler = new AddProductCommandHandler();
 
-        AddProductCommand addProductCommand = mock(AddProductCommand.class);
-
         addProductCommandHandler.handle(addProductCommand);
 
     }
 
+    @Test
+    public void handle_checkForGetOrderIdCall(){
+
+        AddProductCommandHandler addProductCommandHandler = new AddProductCommandHandler();
+
+        try {
+            addProductCommandHandler.handle(addProductCommand);
+        } catch(NullPointerException e){
+            //this is expected
+        }
+
+        verify(addProductCommand, times(1)).getOrderId();
+
+    }
 
 }
