97e31598ecf38bafe50ccf9d1ae02cc393819dab
Kadłubek <179640@edu.p.lodz.pl>
2016-04-14 13:36:46 +0200

    created test to check for the result of the issauance method

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -4,14 +4,17 @@ import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 import java.util.Arrays;
+import java.util.Date;
 
 import org.junit.Test;
 import org.hamcrest.Matchers;
 
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.Product;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
-import pl.com.bottega.ecommerce.sales.domain.invoicing.Invoice;
 
 
 public class BookKeeperTest {
@@ -19,38 +22,52 @@ public class BookKeeperTest {
 	@Test
 	public void issuance_InvoiceWithOneItem_resultTest() {
 		InvoiceFactory invoiceFactoryStub = mock(InvoiceFactory.class);
-		Invoice invoiceStub = mock(Invoice.class);
-		
-		
-		when(invoiceFactoryStub.create(any(ClientData.class))).thenReturn(invoiceStub);
+
+		Invoice invoiceToReturn = createSampleInvoice();
+
+		when(invoiceFactoryStub.create(any(ClientData.class))).thenReturn(invoiceToReturn);
 		
 		BookKeeper bookKeeper = new BookKeeper(invoiceFactoryStub);
-		
-		InvoiceRequest invoiceRequest = mock(InvoiceRequest.class);
-		
-		RequestItem requestItem = mock(RequestItem.class);
-		
-		when(requestItem.getProductData().getType()).thenReturn(ProductType.DRUG);
-		
-		
-		
-		when(invoiceRequest.getItems()).thenReturn(Arrays.asList(requestItem));
+
+		InvoiceRequest invoiceRequest = new InvoiceRequest(createSampleClientData());
+		invoiceRequest.add(createSampleRequestItem());
 		
 		TaxPolicy taxPolicy = mock(TaxPolicy.class);
 		
-		Tax taxStub = mock(Tax.class);
+		Tax tax = createSampleTax();
 		
-		when(taxPolicy.calculateTax(any(ProductType.class), any(Money.class))).thenReturn(taxStub);
+		when(taxPolicy.calculateTax(any(ProductType.class), any(Money.class))).thenReturn(tax);
 		
 		Invoice invoice = bookKeeper.issuance(invoiceRequest, taxPolicy);
 		
 		assertThat(invoice.getItems().size(), Matchers.is(1));
 		
 	}
-	
+
 	@Test
 	public void issuance_InvoiceWithTwoItems_behaviorTest() {
 		fail("Not yet implemented");
+	}
+
+	private Tax createSampleTax() {
+		return new Tax(Money.ZERO,"sample tax");
+	}
+
+
+	private Invoice createSampleInvoice(){
+		Invoice invoice = new Invoice(Id.generate(), createSampleClientData());
+		return invoice;
+	}
+
+	private RequestItem createSampleRequestItem(){
+		ProductData productData = mock(ProductData.class); //ProductData constructor is not visible from here
+		when(productData.getType()).thenReturn(ProductType.STANDARD);
+		RequestItem item = new RequestItem(productData,  1, Money.ZERO);
+		return item;
+	}
+
+	private ClientData createSampleClientData(){
+		return  new ClientData(Id.generate(), "Test");
 	}
 
 }
